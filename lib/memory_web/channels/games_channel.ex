acb6f5c86bbfbf6784ca9611ac2e286e8557ca69
miadong <dong.xinm@husky.neu.edu>
2019-10-03 19:46:14 -0400

    add backup agent

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -1,6 +1,7 @@
 defmodule MemoryWeb.GamesChannel do
   use MemoryWeb, :channel
   alias Memory.Game
+  alias Memory.BackupAgent
 
   # def join("games:lobby", payload, socket) do
   #   if authorized?(payload) do
@@ -11,14 +12,15 @@ defmodule MemoryWeb.GamesChannel do
   # end
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-        game = Game.new()
-        socket = socket
-        |> assign(:game, game)
-        |> assign(:name, name)
-        {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
-      else
-        {:error, %{reason: "unauthorized"}}
-      end
+      game = BackupAgent.get(name) || Game.new()
+      BackupAgent.put(name, game)
+      socket = socket
+      |> assign(:game, game)
+      |> assign(:name, name)
+      {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
   end
 
   # Channels can be used in a request/response fashion
@@ -35,20 +37,26 @@ defmodule MemoryWeb.GamesChannel do
   end
 
   def handle_in("click_letter", %{"button_index" => index}, socket) do
+    name = socket.assigns[:name]
     game = Game.on_click_letter(socket.assigns[:game],index)
     socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   def handle_in("clr", %{}, socket) do
-      game = Game.clear(socket.assigns[:game])
-      socket = assign(socket, :game, game)
-      {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+    name = socket.assigns[:name]
+    game = Game.clear(socket.assigns[:game])
+    socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   def handle_in("restart", %{}, socket) do
+    name = socket.assigns[:name]
     game = Game.on_click_restart(socket.assigns[:game])
     socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
